2e96c55e826ecc8fe500931baf4a7d272cbc1fe2
RudroChat22 <127182882+RudroChat22@users.noreply.github.com>
2023-08-16 01:38:56 +0530

    Update index.js

--- index.js
+++ index.js
@@ -23,23 +23,23 @@ function eventListener(){
 function makeSound(key){
     switch(key){
         case 'w':
-            var audio1=new Audio("sounds/tom-1.mp3")
+            var audio1=new Audio("tom-1.mp3")
             audio1.play();
             break;
         case 'a':
-            var audio1=new Audio("sounds/tom-2.mp3")
+            var audio1=new Audio("tom-2.mp3")
             audio1.play();
             break;
         case 's':
-            var audio1=new Audio("sounds/tom-3.mp3")
+            var audio1=new Audio("tom-3.mp3")
             audio1.play();
             break;
         case 'd':
-            var audio1=new Audio("sounds/tom-4.mp3")
+            var audio1=new Audio("tom-4.mp3")
             audio1.play();
             break;
         case 'j':
-           var audio1=new Audio("sounds/crash.mp3")
+           var audio1=new Audio("crash.mp3")
            audio1.play();
            break;
         case 'k':
@@ -47,7 +47,7 @@ function makeSound(key){
            audio1.play();
            break;
         case 'l':
-           var audio1=new Audio("sounds/snare.mp3")
+           var audio1=new Audio("snare.mp3")
            audio1.play();
            break;
 
